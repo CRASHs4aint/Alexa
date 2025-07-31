6d7f992f90a8eed23b96d513689e4ca09e7ad3d6
CRASHs4aint <samratkumarsahu228@gmail.com>
2025-07-31 15:28:00 +0530

    alexa listen

--- main.py
+++ main.py
@@ -2,29 +2,46 @@ import speech_recognition as sr
 import webbrowser
 import pyttsx3
 
+#pip install pocketsphinx
+
 recognizer= sr.Recognizer
 engine = pyttsx3.init()
 
 def speak(text):
-    engine.say("text")
+    engine.say(text)
     engine.runAndWait()
     
-if __name__== "_main_":
+def processCommand(c): 
+    pass   
+    
+if __name__== "__main__":
     speak("Initializing Alexa.......")
     while True:
          #listen for the woke word "alexa"
          # obtain audio from the microphone
          r = sr.Recognizer()
-         with sr.Microphone() as source:
-          print("Say something!")
-          audio = r.listen(source)
-
-    # recognize speech using Sphinx
+         
+         
+         
+    print("recognizing..")
+    
     try:
-        print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-    except sr.UnknownValueError:
-        print("Sphinx could not understand audio")
-    except sr.RequestError as e:
-        print("Sphinx error; {0}".format(e))
+        with sr.Microphone() as source:
+          print("Listening...")
+          audio = r.listen(source,timeout=2,phrase_time_limit=1)
+        word = r.recognize_google(audio)
+        if(word.lower() == "alexa"):
+           speak("ya")
+           #listen for command 
+           with sr.Microphone() as source:
+            print("Alexa Active...")
+           audio = r.listen(source)
+           command = r.recognize_google(audio)
+           
+           processCommand()
+           
+           
+    except Exception as e:
+        print(" Error; {0}".format(e))
 
-   
+    
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-speechrecogination
+ r = sr.Recognizer()
+        #  with sr.Microphone() as source:
+        #   print("Listening...")
+        #   audio = r.listen(source)
