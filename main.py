1c5f12831097b7a13f5f71832cd9c456733fa7ff
CRASHs4aint <samratkumarsahu228@gmail.com>
2025-08-05 17:24:29 +0530

    modify

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import musicLibrary
 
 #pip install pocketsphinx
 
-recognizer= sr.Recognizer
+recognizer= sr.Recognizer()
 engine = pyttsx3.init()
 
 def speak(text):
@@ -30,32 +30,38 @@ def processCommand(c):
     
 if __name__== "__main__":
     speak("Initializing Alexa.......")
-    while True:
-         #listen for the woke word "alexa"
-         # obtain audio from the microphone
-         r = sr.Recognizer()
-         
-         
-         
-    print("recognizing..")
     
-    try:
-        with sr.Microphone() as source:
-          print("Listening...")
-          audio = r.listen(source,timeout=2,phrase_time_limit=1)
+    #listen for the woke word "alexa"
+         # obtain audio from the microphone
+    
+    while True:
+        
+        try:
+          r = sr.Recognizer()
+          with sr.Microphone() as source:
+           print("Listening for wake word...")
+          audio = r.listen(source,timeout=3,phrase_time_limit=2)
+        except Exception as e:
+         print(f"Error: {e}") 
+    
         word = r.recognize_google(audio)
+        print(f"Heard:{word}")
         if(word.lower() == "alexa"):
            speak("ya")
            #listen for command 
            with sr.Microphone() as source:
             print("Alexa Active...")
-           audio = r.listen(source)
+           audio = r.listen(source ,Timeout=5,phrase_time_limit=5)
+           
            command = r.recognize_google(audio)
+           print(f"command:{command}")
+#            processCommand(command)
+# except Exception as e:
+# print(f"Error: {e}")   
            
-           processCommand(command)
-           
-           
-    except Exception as e:
-        print(" Error; {0}".format(e))
+#         except sr.waitTimeoutError:
+#         continue #Ignore and continue listening
+#  except sr.UnknownValueError:
+# print("Sorry, I didn't catch that.")
 
     
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,1 @@
- r = sr.Recognizer()
-        #  with sr.Microphone() as source:
-        #   print("Listening...")
-        #   audio = r.listen(source)
+speech_recognition
